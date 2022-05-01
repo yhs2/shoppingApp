4f1acf73ad35eef0878b85ee499c76e200cdb8b8
15yhcs <55156646+15yhcs@users.noreply.github.com>
2022-05-01 10:08:44 -0700

    Transform to shopping app

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test', 'this is a test desc', 'https://www.saveur.com/uploads/2020/11/20/Y7RZPFZEERAZVHJ2VHC2RXMEEY.jpg?quality=85&width=540'),
+    new Recipe('A test', 'this is a test desc', 'https://www.saveur.com/uploads/2020/11/20/Y7RZPFZEERAZVHJ2VHC2RXMEEY.jpg?quality=85&width=540')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
